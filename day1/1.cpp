a559e19129319550c1c8c235cf242cc7e551526c
David Fraser <davidfraser@Davids-MBP.lan>
2022-12-01 07:18:48 -0700

    d1

---
+++ day1/1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	ifstream f("input.txt");
+	string c;
+	string::size_type sz;
+	int m1,m2,m3,current = 0;
+	while (getline(f, c)) {
+		if (c == "") { 
+			if (current > m1) {
+				m2 = m1;
+				m3 = m2;
+				m1 = current;
+			} else if (current > m2) {
+				m3 = m2;
+				m2 = current;
+			} else if (current > m3) {
+				m3 = current;
+			}
+			current = 0;
+		}
+		else 
+			current += stoi(c, &sz);
+	}
+	cout << m1 + m2 + m3 << endl;
+	f.close();
+	return 0;
+}
