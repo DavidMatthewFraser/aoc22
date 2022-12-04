09c778736f58f2f19bf9f019d3afe7e43fec8bb1
David Fraser <davidfraser@Davids-MBP-2.lan>
2022-12-04 10:14:46 -0700

    change boolean expression after watching Jonathan Paulson

--- 4.cpp
+++ 4.cpp
@@ -8,7 +8,7 @@ int main()
     int a1, a2, b1, b2;
     char c;
     while (f >> a1 >> c >> a2 >> c >> b1 >> c >> b2) { 
-        if ((a1 < b1 && a2 < b1) || (a1 > b1 && a1 > b2)) { continue; }
+        if (a2 < b1 || a1 > b2) { continue; }
         total++;
     }
     cout << total << endl;
